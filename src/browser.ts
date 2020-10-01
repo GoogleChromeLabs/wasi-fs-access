4747b33a971b9b3241855495adf3a2118ac61fff
Ingvar Stepanyan <rreverser@google.com>
2020-10-01 15:47:52 +0100

    Print a warning in unsupported browsers

--- src/browser.ts
+++ src/browser.ts
@@ -21,31 +21,73 @@ declare const FitAddon: typeof import('xterm-addon-fit');
 declare const WebLinksAddon: typeof import('xterm-addon-web-links');
 
 // Polyfills for new APIs on stable Chrome.
-navigator.storage.getDirectory ??= () =>
-  FileSystemDirectoryHandle.getSystemDirectory({
-    type: 'sandbox'
-  });
-FileSystemDirectoryHandle.prototype.getDirectoryHandle ??=
-  FileSystemDirectoryHandle.prototype.getDirectory;
-FileSystemDirectoryHandle.prototype.getFileHandle ??=
-  FileSystemDirectoryHandle.prototype.getFile;
-FileSystemDirectoryHandle.prototype.values ??= function (this: FileSystemDirectoryHandle) {
-  return this.getEntries()[Symbol.asyncIterator]();
-};
-globalThis.showDirectoryPicker ??= () =>
-  chooseFileSystemEntries({
-    type: 'open-directory'
-  });
+let hasSupport = true;
+try {
+  navigator.storage.getDirectory ??= () =>
+    FileSystemDirectoryHandle.getSystemDirectory({
+      type: 'sandbox'
+    });
+  FileSystemDirectoryHandle.prototype.getDirectoryHandle ??=
+    FileSystemDirectoryHandle.prototype.getDirectory;
+  FileSystemDirectoryHandle.prototype.getFileHandle ??=
+    FileSystemDirectoryHandle.prototype.getFile;
+  FileSystemDirectoryHandle.prototype.values ??= function (
+    this: FileSystemDirectoryHandle
+  ) {
+    return this.getEntries()[Symbol.asyncIterator]();
+  };
+  globalThis.showDirectoryPicker ??= () =>
+    chooseFileSystemEntries({
+      type: 'open-directory'
+    });
+} catch {
+  hasSupport = false;
+}
 
 (async () => {
   const module = WebAssembly.compileStreaming(fetch('./uutils.async.wasm'));
 
   let term = new Terminal();
+
   let fitAddon = new FitAddon.FitAddon();
   term.loadAddon(fitAddon);
+
   let localEcho = new LocalEchoController();
   term.loadAddon(localEcho);
+
   term.loadAddon(new WebLinksAddon.WebLinksAddon());
+
+  term.open(document.body);
+  fitAddon.fit();
+  onresize = () => fitAddon.fit();
+
+  term.writeln(
+    '# Welcome to a shell powered by WebAssembly, WASI, Asyncify and File System Access API!'
+  );
+  term.writeln(
+    '# Github repo with the source code and details: https://github.com/GoogleChromeLabs/wasi-fs-access'
+  );
+  term.writeln('');
+  if (!hasSupport) {
+    term.writeln('Looks like your browser doesn\'t have support for the File System Access API.');
+    term.writeln('Please try a Chromium-based browser such as Google Chrome or Microsoft Edge.');
+    return;
+  }
+  term.writeln(
+    '# Right now you have /sandbox mounted to a persistent sandbox filesystem:'
+  );
+  term.writeln('$ df -a');
+  term.writeln(
+    'Filesystem          1k-blocks         Used    Available  Use% Mounted on'
+  );
+  term.writeln(
+    'wasi                        0            0            0     - /sandbox'
+  );
+  term.writeln('');
+  term.writeln('# To mount a real directory, type "mount /some/path".');
+  term.writeln('# To view a list of other commands, type "help".');
+  term.writeln('# Happy hacking!');
+  term.writeln('');
 
   const stdout = {
     write(data: Uint8Array) {
@@ -63,34 +105,6 @@ globalThis.showDirectoryPicker ??= () =>
 
   let preOpen: Record<string, FileSystemDirectoryHandle> = {};
   preOpen['/sandbox'] = await navigator.storage.getDirectory();
-
-  term.open(document.body);
-
-  term.writeln(
-    '# Welcome to a shell powered by WebAssembly, WASI, Asyncify and File System Access API!'
-  );
-  term.writeln(
-    '# Github repo with the source code and details: https://github.com/GoogleChromeLabs/wasi-fs-access'
-  );
-  term.writeln('');
-  term.writeln(
-    '# Right now you have /sandbox mounted to a persistent sandbox filesystem:'
-  );
-  term.writeln('$ df -a');
-  term.writeln(
-    'Filesystem          1k-blocks         Used    Available  Use% Mounted on'
-  );
-  term.writeln(
-    'wasi                        0            0            0     - /sandbox'
-  );
-  term.writeln('');
-  term.writeln('# To mount a real directory, type "mount /some/path".');
-  term.writeln('# To view a list of other commands, type "help".');
-  term.writeln('# Happy hacking!');
-  term.writeln('');
-
-  fitAddon.fit();
-  onresize = () => fitAddon.fit();
 
   while (true) {
     let line = await localEcho.read('$ ');
@@ -110,7 +124,9 @@ globalThis.showDirectoryPicker ??= () =>
         continue;
       }
       if (args[0] === 'cd') {
-        term.writeln('Unfortunately, WASI doesn\'t have a concept of current working directory.');
+        term.writeln(
+          "Unfortunately, WASI doesn't have a concept of current working directory."
+        );
         term.writeln('Please pass absolute paths to all commands.');
         continue;
       }
