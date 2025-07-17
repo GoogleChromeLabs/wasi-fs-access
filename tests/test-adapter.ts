fa1a98aaf6b1964193f4a91adc2d78ff7fa69d43
Ingvar Stepanyan <me@rreverser.com>
2025-07-17 19:51:40 +0100

    Don't close bindings
    If some file is left open, let Node.js warn us - it's a test runner, after all.

--- tests/test-adapter.ts
+++ tests/test-adapter.ts
@@ -4,52 +4,48 @@ import { readFile } from 'node:fs/promises';
 import { parseArgs } from 'node:util';
 
 const {
-  values: {
-    version,
-    'test-file': testFile,
-    'arg': args,
-    'env': envVars,
-    'dir': dirs,
-  }
+  values: { version, 'test-file': testFile, arg: args, env: envVars, dir: dirs }
 } = parseArgs({
   options: {
     version: {
       type: 'boolean',
       short: 'v',
-      default: false,
+      default: false
     },
     'test-file': {
-      type: 'string',
+      type: 'string'
     },
-    'arg': {
-      type: 'string',
-      multiple: true,
-      default: [],
-    },
-    'env': {
+    arg: {
       type: 'string',
       multiple: true,
-      default: [],
+      default: []
     },
-    'dir': {
+    env: {
       type: 'string',
       multiple: true,
-      default: [],
+      default: []
     },
+    dir: {
+      type: 'string',
+      multiple: true,
+      default: []
+    }
   }
 });
 
 if (version) {
-  const { name, version } = JSON.parse(await readFile(new URL('../package.json', import.meta.url), 'utf-8'));
+  const { name, version } = JSON.parse(
+    await readFile(new URL('../package.json', import.meta.url), 'utf-8')
+  );
   console.log(`${name} v${version}`);
   process.exit(0);
 }
 
 assert.ok(testFile, 'Test file must be specified with --test-file');
 
-await using bindings = new Bindings({
+let bindings = new Bindings({
   args,
-  env: envVars,
+  env: envVars
 });
 
 for (const dir of dirs) {
