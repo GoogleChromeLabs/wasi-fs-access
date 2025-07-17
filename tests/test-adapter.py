ff2fe87a11dfe48d035f0d6b530e6be35fd439b4
Ingvar Stepanyan <me@rreverser.com>
2025-07-17 11:29:47 +0100

    Don't --inspect tests by default

--- tests/test-adapter.py
+++ tests/test-adapter.py
@@ -5,7 +5,7 @@ from pathlib import Path
 tests_dir = Path(__file__).parent
 
 try:
-	subprocess.run(["node", "--inspect", "--experimental-wasm-jspi", "--import", "tsx", tests_dir / "test-adapter.ts", *sys.argv[1:]], check=True)
+	subprocess.run(["node", "--experimental-wasm-jspi", "--import", "tsx", tests_dir / "test-adapter.ts", *sys.argv[1:], "--env", "NO_FD_ALLOCATE=1"], check=True)
 except:
 	# If the test fails, it keeps garbage around which results in different failures for subsequent tests. Clean it up.
 	subprocess.run(["git", "-C", tests_dir / "wasi-testsuite", "clean", "-df"], check=True)
